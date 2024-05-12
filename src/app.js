acd7d714f95d5b20f9faac066dad6e201a61ef47
Dhiraj Kumar Kushwaha <officaldhirajdj@gmail.com>
2024-05-12 13:12:42 +0530

    hi

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import KanbanBoard from "./kanbanBoard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Day tasks</h1>
+      <KanbanBoard />
+    </div>
+  );
+}
